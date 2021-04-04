ee31f8f6c414c116cc42dab1970de9d843538537
aryan612 <81935221+aryan612@users.noreply.github.com>
2021-04-04 23:20:22 +0000

    Create aryankh4n.py
    pkg update
    pkg upgrade
    pkg install git
    pkg insall python
    pkg install python2
    pip2 install requests mechanize
    pip2 install bs4
    git clone https://github.com/aryan612/aryankhan.git
    cd aryan
    bash Abm-install.sh

---
+++ aryankh4n.py
@@ -0,0 +1,8 @@
+.o88b. db       .d88b.  d8b   db d88888b                       d8P  Y8 88      .8P  Y8. 888o  88 88'
+    8P      88      88    88 88V8o 88 88ooooo                       8b      88      88    88 88 V8o88 88~~~~~
+    Y8b  d8 88booo. `8b  d8' 88  V888 88.
+     `Y88P' Y88888P  `Y88P'  VP   V8P Y88888P                   
+--------------------------------------------------
+[➤] Author   : Aryan
+[➤] Github   : https://github.com/aryan612/aryan
+[➤] Fb Page  : https://www.facebook.com/Iťş-Ąřyåņ-Ķhaň-109589910511225/
